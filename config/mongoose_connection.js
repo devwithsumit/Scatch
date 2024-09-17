9af435fd6303d54863a286bbdc3941fd339d3540
webwithsumit <sumit2410kushwaha@gmail.com>
2024-09-17 10:49:43 +0530

    mongoose connection

--- config/mongoose_connection.js
+++ config/mongoose_connection.js
@@ -1,3 +1,13 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/scatch")
+mongoose
+.connect("mongodb://localhost:27017/scatch")
+.then(function(){
+    console.log('database connected !');
+})
+.catch(function(err){
+    console.log('connection error', err);
+})
+
+
+// module.exports = mongoose.connection;
