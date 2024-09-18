1e051b5cdcbe8d5b3d28051e15e1014284e227eb
webwithsumit <sumit2410kushwaha@gmail.com>
2024-09-18 21:02:30 +0530

    routers settled and some other requirements done

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require("express")
 const app = express()
 
+const db = require("./config/mongoose_connection")
+
 const cookieParser = require("cookie-parser")
 const path = require("path")
+const usersRouter = require("./routes/usersRouter")
+const productsRouter = require("./routes/productsRouter")
+const ownersRouter = require("./routes/ownersRouter")
 
-const User = require("./models/user")
+require("dotenv").config()
 
 app.set("view engine", "ejs")
 app.use(express.json())
 app.use(cookieParser())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "public")))
 
-app.get("/",async function(req, res){
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/owner", ownersRouter);
+
+
+app.get("/", async function (req, res) {
     res.send('hey');
 })
 
